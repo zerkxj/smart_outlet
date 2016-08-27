a1b572c53f2ad81934009213949b7789a7353638
Carlos Chen <zerkxj@gmail>
2016-08-27 09:37:18 +0800

    description: add fingerprint operation
    modify file:
    
      1. smart_outlet.ino
        1. add fingerprint operation

--- smart_outlet.ino
+++ smart_outlet.ino
@@ -1,14 +1,36 @@
 #include "ade7753.h"
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
 #include "SPI.h"
 
 long long_eeprom_data = 0;
 byte clr;
 
+//  // pin #10 is IN from sensor (GREEN wire)
+//  // pin #11 is OUT from arduino  (WHITE wire)
+SoftwareSerial serial_fingerprint(10, 11);
+
 ADE7753 ade7753;
 
+Adafruit_Fingerprint fingerprint = Adafruit_Fingerprint(&serial_fingerprint);
+
+int getFingerprintIDez();
+boolean led_switch = false;
+
 void setup () {
+
   Serial.begin(9600);
 
+
+  fingerprint.begin(57600); // set the data rate for the sensor serial port
+  if (fingerprint.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+
+/*
   // setup SPI I/F for ADE7753
   pinMode(MOSI, OUTPUT);
   pinMode(MISO, INPUT);
@@ -58,13 +80,114 @@ void setup () {
   long_eeprom_data = ade7753.rd_reg(RSTSTATUS, 2); // reset interrupt status
   Serial.println("clear interrupt first...");
   delay(10000);
+*/
 
   Serial.println("initial done...");
 }
 
 void loop () {
-   long_eeprom_data = ade7753.rd_reg(MODE, 2);
-   Serial.println("MODE CHECK");
-   Serial.println(long_eeprom_data, HEX);
-   delay(1000);
+/*
+  long_eeprom_data = ade7753.rd_reg(MODE, 2);
+  Serial.println("MODE CHECK");
+  Serial.println(long_eeprom_data, HEX);
+  delay(1000);
+*/
+
+  int id_finger_print;
+  id_finger_print = getFingerprintIDez();
+  if (id_finger_print >= 0) {
+    if (led_switch)
+      digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
+    else
+      digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+    led_switch = !led_switch;
+  }
+
+  delay(50); //don't ned to run this at full speed.
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = fingerprint.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = fingerprint.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK converted!
+  p = fingerprint.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(fingerprint.fingerID);
+  Serial.print(" with confidence of "); Serial.println(fingerprint.confidence);
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = fingerprint.getImage();
+  Serial.print("get image"); Serial.print(p);Serial.print("\n");
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = fingerprint.image2Tz();
+    Serial.print("image2Tz"); Serial.print(p);Serial.print("\n");
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = fingerprint.fingerFastSearch();
+    Serial.print("fast search"); Serial.print(p);Serial.print("\n");
+  if (p != FINGERPRINT_OK)  return -1;
+
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(fingerprint.fingerID);
+  Serial.print(" with confidence of ");
+  Serial.println(fingerprint.confidence);
+  return fingerprint.fingerID;
 }
